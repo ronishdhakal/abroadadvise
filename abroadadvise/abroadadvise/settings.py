980119959f28ed2e025c98145b082a45880cfe62
ronishdhakal <ronishdhakal1@gmail.com>
2025-05-24 09:31:15 +0545

    Sepeare .env for Database Backend

--- abroadadvise/abroadadvise/settings.py
+++ abroadadvise/abroadadvise/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'abroadadvise.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'abroadadvise_db',
-        'USER': 'postgres',
-        'PASSWORD': 'Abroad@@##77',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME', 'abroadadvise_db'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
